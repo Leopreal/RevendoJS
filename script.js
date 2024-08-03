90d566ccc9429354490b4c8d535e928397eee2cc
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-08-03 16:00:09 -0300

    revendo mais sobre js

--- script.js
+++ script.js
@@ -52,11 +52,89 @@ const user = {
     nome: "LEO",
     falarUserName() {
         var self = this
-        setTimeout(function () {
-            console.log(self)
-            console.log("Username:" + self.nome)
-        }, 500)
+        // setTimeout(function () {
+        //     console.log(self)
+        //     console.log("Username:" + self.nome)
+        // }, 500)
     }
 }
 
 user.falarUserName()
+
+const arr = [1,2,3,4]
+
+const MaioresNumero = arr.filter((numero) => {
+    if(numero >= 3) {
+        return numero
+    }
+
+})
+
+console.log(MaioresNumero)
+
+const users = [
+    {nome: "Leo", disponivel: true},
+    {nome: "Lucas", disponivel: false},
+    {nome: "mateus", disponivel: true},
+    {nome: "Hugo", disponivel: false}
+]
+
+const UsuariosDisponiveis = users.filter((user) => user.disponivel)
+
+console.log(UsuariosDisponiveis)
+
+// map
+
+const produtos = [
+    {nome: "Pano", preco: 19.99, categoria: "limpeza"},
+    {nome: "camiseta", preco: 20, categoria: "roupas"},
+    {nome: "calca", preco: 50, categoria: "roupas"}
+
+]
+
+produtos.map((item) => {
+    if(item.categoria === 'roupas') {
+        item.A_venda = true
+    }
+})
+
+console.log(produtos)
+
+const frutas = ["maca", "pera", "mamao"]
+
+const [f1, f2, f3] = frutas
+
+console.log(f1)
+
+const produto = {
+    nome: "camiseta",
+    preco: 30,
+    cor: "preto"
+}
+
+const {
+    nome: DetalhesProduto,
+    preco,
+    cor: corProduto
+} = produto
+
+console.log(`o nome do produto é ${DetalhesProduto} com o preco R$${preco} e a cor ${corProduto}`)
+
+// spread operator
+
+const a1 = [1,2,3]
+const a2 = [4,5,6]
+
+const a3 = [...a1, ...a2]
+
+console.log(a3)
+
+
+const NomeCarro = {nome: 'gol'}
+const MarcaCarro = {marca: 'vw'}
+const kms = {km: 1000}
+
+
+const carro = {...NomeCarro, ...MarcaCarro, ...kms}
+
+console.log(carro)
